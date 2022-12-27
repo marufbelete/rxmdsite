3544a31b053f95fdf3c590953fda2871e7f9f469
Jacob Briggs <jdbriggs81@gmail.com>
2022-12-27 14:28:58 -0600

    Elipsion (#26)
    * merging main into elipsion
    
    * merging elipsion and main
    
    * merging elipsion and main
    
    * fixed routes and navbar and styled login with google button
    
    * finished changes to login and register

--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -27,20 +27,24 @@ module.exports = (app) => {
     res.redirect("https://form.jotform.com/212587273457161");
   });
 
-  router.get("/shop", function (req, res) {
-    res.render(path.join(__dirname, "..", "/views/pages/shop"));
-  });
+  // router.get("/shop", function (req, res) {
+  //   res.render(path.join(__dirname, "..", "/views/pages/shop"));
+  // });
 
-  router.get("/cart", authenticateJWT,function (req, res) {
-    res.render(path.join(__dirname, "..", "/views/pages/shop-cart"));
-  });
+  // router.get("/cart", authenticateJWT,function (req, res) {
+  //   res.render(path.join(__dirname, "..", "/views/pages/shop-cart"));
+  // });
 
   router.get("/checkout", function (req, res) {
     res.render(path.join(__dirname, "..", "/views/pages/shop-checkout"));
   });
 
-  router.get("/products/details", function (req, res) {
-    res.render(path.join(__dirname, "..", "/views/pages/shop-product-details"));
+  // router.get("/products/details", function (req, res) {
+  //   res.render(path.join(__dirname, "..", "/views/pages/shop-product-details"));
+  // });
+
+  router.get("/forgotpw", function (req, res) {
+    res.render(path.join(__dirname, "..", "/views/pages/forgotPassword"));
   });
 
   router.get("/forgotpw", function (req, res) {
@@ -65,6 +69,10 @@ module.exports = (app) => {
 
   router.get("/privacypolicy", function (req, res) {
     res.render(path.join(__dirname, "..", "/views/pages/privacyPolicy"));
+  });
+
+  router.get("/tos", function (req, res) {
+    res.render(path.join(__dirname, "..", "/views/pages/tos"));
   });
 
   app.use("/", router);
