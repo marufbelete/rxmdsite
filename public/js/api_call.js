37cc5f33ddcf7712bf9f453986ebf0adf9372703
marufbelete9@gmail.com <marufbelete9@gmail.com>
2023-01-02 07:48:41 +0300

    before merge

--- public/js/api_call.js
+++ public/js/api_call.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  // const base_url = "http://localhost:7000";
-  const base_url="https://rxmdsite-production.up.railway.app"
+  const base_url = "http://localhost:7000";
+  // const base_url="https://rxmdsite-production.up.railway.app"
 
   $("#populate").on("click", function () {
     $("#user-table-body").empty()
@@ -26,12 +26,12 @@ $(document).ready(function () {
               <td>${user.intake||''}</td>
               <td>${new Date(user.createdAt).toLocaleDateString()||''}</td>
               <td>
-                <span class="delete-user" data-toggle="modal" data-target="#user_delete_modal"
+                <span class="delete-user-icon" data-bs-toggle="modal" data-bs-target="#delete_user"
                  data-id="${user.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
               </span>
-                <span class="edit-user" data-id="${user.id}">
+                <span class="edit-user-icon" data-id="${user.id}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                 </svg>
@@ -374,6 +374,49 @@ $('#update-product-button').on("click", function () {
 
   });
 });
+//edit user
+$('#update-user-info').on("click", function () {
+  const user_id = $('#update-user-id').data('id');
+  const first_name = $('#update-user-id').data('id');
+  const last_name = $('#update-user-id').data('id');
+  const address = $('#update-user-id').data('id');
+  const city = $('#update-user-id').data('id');
+  const state = $('#update-user-id').data('id');
+  const zip = $('#update-user-id').data('id');
+  const phone = $('#update-user-id').data('id');
+  const jotformfilled = $('#update-user-id').data('id');
+  const addressline2=  $('#update-user-id').data('id');
+  console.log(selected_id)
+  $("#update_product_text").addClass("d-none");
+  $("#update_product_text_spin").removeClass("d-none");
+  $.ajax({
+    url: `${base_url}/updateuser/${user_id}`,
+    data: {first_name,last_name,},
+    method: "PUT",
+    success: function () {
+      $('#product_notify').text("product updated successfully").
+      removeClass('d-none alert alert-danger').addClass('alert alert-primary');
+      setTimeout(function() {
+        $('#product_notify').
+        addClass('d-none');
+    },3000);
+      $("#update_product_text").removeClass("d-none");
+      $("#update_product_text_spin").addClass("d-none");
+    },
+    error: function (data) {
+      $('#product_notify').text("something went wrong, please try again").
+      removeClass('d-none alert alert-primary').
+      addClass('alert alert-danger');
+      setTimeout(function() {
+        $('#product_notify').
+        addClass('d-none');
+    },3000);
+      $("#update_product_text").removeClass("d-none");
+      $("#update_product_text_spin").addClass("d-none");
+    },
+
+  });
+});
 //seacrh user
 $('#user_search').on("click", function (event) {
   event.preventDefault();
@@ -381,8 +424,6 @@ $('#user_search').on("click", function (event) {
   const email=$('div').children('#edit-email').val()
   // const loged_at= $('#loged_at').val() 
   // const loged_out= $('#loged_out_at').val() 
-  // const price=$('div').children('#product-price').val()
-  // console.log(selected_id)
  let searchString
   if(!email&&!user_name){
     $('#user_search_notify').text("please fill atleast one query for usersearch")
@@ -422,11 +463,11 @@ $('#user_search').on("click", function (event) {
             <td>${user.intake||''}</td>
             <td>${new Date(user.createdAt).toLocaleDateString()||''}</td>
             <td>
-              <span class="delete-user"  data-id="${user.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <span class="delete-user-icon" data-id="${user.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
               <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
             </span>
-              <span class="edit-user" data-id="${user.id}">
+              <span class="edit-user-icon" data-id="${user.id}">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
               </svg>
@@ -449,6 +490,19 @@ $('#user_search').on("click", function (event) {
     },
   });
 });
+//delete user
+$('#delete-confirmation').on('click',function(){
+  console.log($(this).data('id'))
+  $("#delete_user_text").addClass("d-none");
+  $("#delete_user_text_spin").removeClass("d-none");
+})
+$(document).on("click",'.delete-user-icon',function(){
+  console.log("clicked")
+  const id=$(this).data('id')
+  console.log(id)
+  $('#delete-confirmation').data('id',id)
+})
+
 
 $('#cancel-edit-user').on('click',function(){
   $('div').children('#edit-email').val('')
