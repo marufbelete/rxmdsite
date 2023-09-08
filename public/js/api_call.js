fa749cf62b22f24832af45368f043076bf76e826
marufbelete9@gmail.com <marufbelete9@gmail.com>
2023-09-08 08:12:15 +0300

    test url

--- public/js/api_call.js
+++ public/js/api_call.js
@@ -1806,13 +1806,17 @@ $("#continue-schedule").click(function(event){
   event.preventDefault(); // prevent the form from submitting normally
   $("#continue-schedule_text").addClass("d-none");
   $("#continue-schedule_text_spin").removeClass("d-none");
+  const referrer = document.referrer;
+
+// Log the value to the console
+alert(`HTTP Referer Header: ${referrer}`);
 $.ajax({
   url: `${base_url}/checkappt`,
   method: "GET",
   contentType: 'application/json',
   success: function (data) {
     //redirect to given url
-    const referrer = document.referrer;
+    // const referrer = document.referrer;
 
 // Log the value to the console
 alert(`HTTP Referer Header: ${referrer}`);
